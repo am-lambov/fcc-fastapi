c85475b36bc8a2fa8e2fd7d40f9e357271a89825
Lambov <am.lambov@gmail.com>
2024-09-24 11:06:47 +0300

    Refactor delete_post endpoint following best practices for clean code

--- app/routers/post.py
+++ app/routers/post.py
@@ -48,40 +48,47 @@ def create_posts(
     return new_post
 
 
-@router.delete("/{post_id}", status_code=status.HTTP_200_OK)
+@router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(
     post_id: int,
     db: Session = Depends(get_db),
     current_user: models.User = Depends(oauth2.get_current_user),
 ):
     post = fetch_post_from_db(db, post_id)
-    check_authorship(current_user, post)
+    check_permission_to_delete_post(current_user, post)
     delete_post_from_db(db, post_id)
 
-    return {"message": f"Post with id: {post_id} was successfully deleted."}
 
-
-def check_authorship(current_user, post):
-    if not post.author_id == current_user.id:
+def check_permission_to_delete_post(user, post):
+    if not is_author(user, post):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="You can delete only your own posts.",
         )
 
 
+def is_author(user, post) -> bool:
+    return post.author_id == user.id
+
+
 def delete_post_from_db(db, post_id):
-    db.query(models.Post).filter(models.Post.id == post_id).delete(synchronize_session=False)
+    db.query(models.Post).filter(models.Post.id == post_id).delete(
+        synchronize_session=False
+    )
     db.commit()
 
 
 def fetch_post_from_db(db, post_id) -> models.Post:
-    post: Optional[models.Post] = db.query(models.Post).filter(models.Post.id == post_id).first()
+    post: Optional[models.Post] = (
+        db.query(models.Post).filter(models.Post.id == post_id).first()
+    )
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {post_id} was not found.",
         )
     return post
+
 
 @router.put("/{post_id}", response_model=schemas.PostResponse)
 async def update_put_post(
